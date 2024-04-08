87d9cf2242822bea039005563b264914c8342fb0
Smeleks <alexpetrosyan787@gmail.com>
2024-04-09 00:02:10 +0400

    Initial commit

--- public/main.js
+++ public/main.js
@@ -1,5 +1,27 @@
 const socket = io();
 
-socket.on('message', (serverData)=>{
-    console.log(serverData);
+socket.on('message', (scoketData) => {
+    console.log(scoketData);
 });
+
+const chatForm = document.getElementById('chat-form');
+
+function outputMsg (m){ 
+	const div = document.createElement('div') 
+	const container = document.querySelector(".chat-messages"); 	
+    div.classList.add('message');
+	
+    div.innerHTML = `<p class='meta'>John 	<span>12:20pm</span></p><p class='text'>${m}</p>`; 	container.appendChild(div) ;
+
+    chatForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const msg = e.target.elements.msg.value;
+        e.target.elements.msg.value = '';
+        socket.emit('chatMsg', msg)
+    });
+    
+    socket.on('message', (scoketData) => {
+        outputMsg(scoketData);
+    });
+};
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require('path');
 const http = require('http');
+const socket = require('socket.io');
 
 const app = express();
 const PORT = 3000;
@@ -8,15 +9,22 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
+const io = socket(server);
 
 io.on('connection', (socket) => {
     console.log('User Connected Successful!');
 
+    socket.emit('message', 'Hello, New User!');
+    socket.broadcast.emit('message', 'New User Joined!');
+
+    socket.on('chatMsg', (m) => {
+        io.emit('message', m);
+    });
+
     socket.on('disconnect', () => {
-        console.log('User Disconnected!');
+        io.emit('message', 'User Disconnected!');
     });
 });
-
 
 server.listen(PORT, () => {
 console.log(`server is running on port ${PORT}`);
